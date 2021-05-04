2be74ec6c5f091f8cf4fc91a8c6dba0242b8ec4d
Gumbolab <minduk9703@gmail.com>
2021-05-04 17:26:58 +0700

    push

--- server-login/app-server.js
+++ server-login/app-server.js
@@ -7,9 +7,9 @@ var express = require('express');
 var app = express();
 var server = http.createServer(app);
 // TAI SAO 
-
+const port = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname + './..', 'views')));
-server.listen(3000, "0.0.0.0", function () {
+server.listen(port, "0.0.0.0", function () {
         var addr = server.address();
         console.log("All ready! server is listening at", addr.address + ":" + addr.port);
 });
